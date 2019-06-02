fb912ff3d8e3fbaaeb8fa53cc31ea4e27c9a39f1
abab <ababup1192@gmail.com>
2019-06-02 23:41:46 +0900

    ✨Impl image counter

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,92 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, footer, h1, img, node, p, text)
-import Html.Attributes exposing (class, src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
+
+-- ---------------------------
+-- MODEL
+-- ---------------------------
+
+
+type alias Model =
+    { logoSize : Float
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { logoSize = 10.0 }, Cmd.none )
+
+
+
+-- ---------------------------
+-- UPDATE
+-- ---------------------------
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        { logoSize } =
+            model
+    in
+    case msg of
+        Increment ->
+            ( { model | logoSize = logoSize + 0.2 }, Cmd.none )
+
+        Decrement ->
+            ( { model | logoSize = logoSize - 0.2 }, Cmd.none )
+
+
+
+-- ---------------------------
+-- VIEW
+-- ---------------------------
+
+
+view : Model -> Browser.Document Msg
+view model =
+    let
+        { logoSize } =
+            model
+    in
+    { title = "Elm 0.19 starter"
+    , body =
+        [ header [ class "site-header" ]
+            [ h1 [] [ text "Logo counter" ]
+            ]
+        , main_ []
+            [ img
+                [ src "./images/Elm_logo.png"
+                , style "width" <| String.fromFloat logoSize ++ "em"
+                , style "height" <| String.fromFloat logoSize ++ "em"
+                ]
+                []
+            , article []
+                [ button [ onClick Increment ] [ text "+" ]
+                , button [ onClick Decrement ] [ text "-" ]
+                ]
+            ]
+        ]
+    }
+
+
+
+-- ---------------------------
+-- MAIN
+-- ---------------------------
+
+
+main : Program () Model Msg
 main =
     Browser.document
         { init = init
@@ -12,66 +94,3 @@ main =
         , view = view
         , subscriptions = \_ -> Sub.none
         }
-
-
-
--- MODEL
-
-
-type alias Model =
-    { userState : String
-    }
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model ""
-    , Cmd.none
-    )
-
-
-
--- UPDATE
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- VIEW
-
-
-view : Model -> Browser.Document Msg
-view model =
-    { title = "Elm 0.19 starter"
-    , body =
-        [ siteHeader
-        , node "main"
-            []
-            [ img [ src "./images/Elm_logo.png" ] []
-            ]
-        , siteFooter
-        ]
-    }
-
-
-siteHeader : Html Msg
-siteHeader =
-    Html.header [ class "site-header" ]
-        [ h1 [] [ text "Site Title" ]
-        ]
-
-
-siteFooter : Html Msg
-siteFooter =
-    footer [ class "site-footer" ]
-        [ p [ class "copyright" ] [ text "© 2019 y047aka" ]
-        ]
